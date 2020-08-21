71ab1045c0bfa185763f2493cebbdb7dd3dcb4b3
Sachin Kumar <sachin90uk@gmail.com>
2020-08-21 17:35:33 +0530

    target blank

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -69,7 +69,11 @@ export class User extends Component {
                 <p>{bio}</p>
               </Fragment>
             )}
-            <a href={html_url} className='btn btn-dark my-1'>
+            <a
+              href={html_url}
+              className='btn btn-dark my-1'
+              target='_blank'
+              rel='noopener noreferrer'>
               Visit Github Profile
             </a>
             <ul>
